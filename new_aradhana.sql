05a11fb7b93ce7832259de6ec51c3aa5c3aa3101
kaweendras <salitha.wijerathna@gmail.com>
2021-07-30 16:26:09 +0530

    new db

--- new_aradhana.sql
+++ new_aradhana.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2021 at 02:12 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Generation Time: Jul 30, 2021 at 12:55 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,35 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `advisor`
---
-
-CREATE TABLE `advisor` (
-  `id` int(11) NOT NULL,
-  `fname` varchar(20) DEFAULT NULL,
-  `lname` varchar(20) DEFAULT NULL,
-  `dob` varchar(20) DEFAULT NULL,
-  `gender` varchar(15) NOT NULL,
-  `email` varchar(20) DEFAULT NULL,
-  `tp` varchar(20) DEFAULT NULL,
-  `address` varchar(20) DEFAULT NULL,
-  `nic` varchar(20) DEFAULT NULL,
-  `password` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `advisor`
---
-
-INSERT INTO `advisor` (`id`, `fname`, `lname`, `dob`, `gender`, `email`, `tp`, `address`, `nic`, `password`) VALUES
-(1, 'ATestFname', 'ATestLname', '2021-07-06', '', 'test@test.com', '07111111111', 'Kurunegala', '111111121v', '12345678'),
-(2, 'ATestFname', 'ATestLname', '2021-07-26', '', 'test@test.com', '07111111111', 'Kurunegala', '111111121v', '12345678'),
-(3, 'iu', 'ii', '2021-07-13', '', 'test@test.com', '123dgbny5', 'ewgwrhq', '8878686686868', '12345678'),
-(4, 'rfsd', 'dsgsg', '2021-07-16', 'Female', 'sdgd@df.bom', 'wegdgw', 'wewe', 'w3241', '12345678');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `employee`
 --
 
@@ -62,7 +33,7 @@ CREATE TABLE `employee` (
   `lname` varchar(20) DEFAULT NULL,
   `dob` varchar(20) DEFAULT NULL,
   `gender` varchar(15) NOT NULL,
-  `email` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
   `tp` varchar(20) DEFAULT NULL,
   `address` varchar(20) DEFAULT NULL,
   `nic` varchar(20) DEFAULT NULL,
@@ -75,9 +46,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`aid`, `fname`, `lname`, `dob`, `gender`, `email`, `tp`, `address`, `nic`, `password`, `role`) VALUES
-(4, 'dfsg', 'rtwrd', '2021-07-14', 'Female', 'edgdg@mbd.codh', '07111111111', 'ibbgmuwa', '8878686686868', '12345678', 'Advisor'),
-(5, 'wdwdwq', 'ATestLname', '2020-12-15', 'Female', 'eddgdg1@mbd.codh', '07167111113', 'Kurunegala', '11234111121v', '12345678', 'Admin'),
-(6, 'TestFname', 'qwrqwr', '2021-07-15', 'Female', 'edgdg@mbd.codh', 'hyi67877988989786756', 'wewe', '=0887879877777766665', '12345678', 'Advisor');
+(4, 'Kamal', 'Perera', '2021-07-14', 'Female', 'k.perera@yahoo.com', '0711111111', 'Ibbagamuwa', '908765678978', '12345678', 'User'),
+(5, 'Saman', 'Kumara', '2020-12-15', 'Female', 'saman1.kumara@hotmail.com', '0716711111', 'Kurunegala', '112341111212', '12345678', 'Admin'),
+(6, 'Sanath', 'Jayasuruya', '2021-07-15', 'Female', 'sanath.jayasuriya@live.com', '0715611111', 'Galgamuwa', '958774678v', '12345678', 'Advisor');
 
 -- --------------------------------------------------------
 
@@ -100,7 +71,8 @@ CREATE TABLE `schedule` (
 INSERT INTO `schedule` (`shid`, `date`, `time1`, `group1`, `aid`) VALUES
 (4, '2021-07-28', '06:33', 'Mid-Day', 6),
 (5, '2021-07-28', '06:33', 'Mid-Day', 6),
-(6, '2021-07-10', '15:59', 'Evening', 6);
+(6, '2021-07-10', '15:59', 'Evening', 6),
+(7, '2021-07-31', '16:54', 'Evening', 6);
 
 -- --------------------------------------------------------
 
@@ -114,7 +86,7 @@ CREATE TABLE `student` (
   `lname` varchar(20) DEFAULT NULL,
   `dob` varchar(20) DEFAULT NULL,
   `gender` varchar(10) NOT NULL,
-  `email` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
   `tp` varchar(20) DEFAULT NULL,
   `address` varchar(20) DEFAULT NULL,
   `nic` varchar(20) DEFAULT NULL,
@@ -129,22 +101,15 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`sid`, `fname`, `lname`, `dob`, `gender`, `email`, `tp`, `address`, `nic`, `password`, `package`, `group1`, `exam`) VALUES
-(3, 'Saman', 'Kumara', '2021-07-06', 'Male', 'test@test.com', '0722222222', 'Wariyapola', '111155521v', '12345678', 'Package 1', 'Mid-Day', 'Pass'),
-(5, 'Yasiru', 'Kaweesha', '1999-01-06', 'Male', 'yasiru3@gmail.com', '0713578562', 'Maho', '990060088v', '12345678', 'Package 2', 'Mid-Day', 'Pass'),
-(6, 'efwtwet', 'ewrwerwer', '2021-07-15', 'Male', 'erwerwerw@erger.vse', '11111111111111111111', 'Kurunegala', '999999999v', '12345678', 'Package 1', 'Morning', 'Fail'),
-(7, 'Yasiru', 'Kaweesha', '1999-01-06', 'Male', 'yasirukaweesha3@gmai', '0713578562', 'Maho', '990060088v', '12345678', 'Package 4', 'Evening', 'Pass'),
-(8, 'Hansika', 'TestLname', '2021-07-10', 'Male', 'sdgd@df.bom', '0713578562', 'Kurunegala', '990060088v', '12345678', 'Package 1', 'Morning', 'Fail'),
-(9, 'Hansika', 'TestLname', '2021-07-10', 'Male', 'sdgd@df.bom', '0713578562', 'Kurunegala', '990060088v', '12345678', 'Package 1', 'Morning', 'Not Attempted');
+(3, 'Saman', 'Kumara', '2021-07-06', 'Male', 'sama.kumara@gmail.com', '0722222222', 'Wariyapola', '890087679v', '12345678', 'Package 1', 'Mid-Day', 'Pass'),
+(5, 'Yasiru', 'Kaweesha', '1999-01-06', 'Male', 'yasiru3@gmail.com', '0713578562', 'Maho', '987745679v', '12345678', 'Package 2', 'Mid-Day', 'Pass'),
+(6, 'Buddhika', 'Prasanna', '2021-07-15', 'Male', 'buddhika22@gmail.com', '0754378565', 'Kurunegala', '978898769v', '12345678', 'Package 1', 'Morning', 'Fail'),
+(7, 'Tharaka', 'Bandara', '1999-01-06', 'Male', 'tharakabandara@gmail.com', '0768778562', 'Maho', '990060088v', '12345678', 'Package 4', 'Evening', 'Pass'),
+(9, 'Wasantha', 'Rathnayaka', '2021-07-10', 'Male', 'wasantha.wasantha@gmail.com', '0701231234', 'Kurunegala', '897780987v', '12345678', 'Package 1', 'Morning', 'Not Attempted');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `advisor`
---
-ALTER TABLE `advisor`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `employee`
@@ -169,12 +134,6 @@ ALTER TABLE `student`
 --
 
 --
--- AUTO_INCREMENT for table `advisor`
---
-ALTER TABLE `advisor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
@@ -184,7 +143,7 @@ ALTER TABLE `employee`
 -- AUTO_INCREMENT for table `schedule`
 --
 ALTER TABLE `schedule`
-  MODIFY `shid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `shid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `student`
